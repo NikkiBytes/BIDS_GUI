2007b2992c764cbbff557a96140e576180b3f54e
gshearrer <grace.shearer@gmail.com>
2018-08-29 14:57:19 -0400

    adding various edits

---
+++ BIDSConversion.py
@@ -0,0 +1,39 @@
+"""
+Author: Nichollette Acosta @ NIBL UNC Chapel Hill
+
+Module for BIDS Conversion GUI
+This is a conversion script the GUI uses to run Heudiconv
+
+"""
+
+from __main__ import *
+
+global STUDYNAME
+global INPUTDIR
+global OUTPUTDIR
+global DICOMPATH
+global MULTISESS
+
+def setSTUDYNAME(x):
+    STUDYNAME = x
+    print(STUDYNAME)
+def setINPUTDIR(x):
+    INPUTDIR = x
+def setOUTPUTDIR(x):
+    OUTPUTDIR = x
+def setDICOMPATH(x):
+    DICOMPATH = x
+def setMULTISESS(x):
+    MULTISESS = x
+
+def runConversion():
+    print("STARTING CONVERSION")
+    ##input bash script here
+
+"""
+def main():
+    runConversion(STUDYNAME)
+
+if __name__ == '__main__':
+    main()
+"""
--- bidsGUI2.py
+++ bidsGUI2.py
@@ -1,11 +1,12 @@
-import sys
 from PyQt5.QtWidgets import QWidget, QLabel,  QAction, QMessageBox, QPushButton, QLineEdit, QInputDialog, QApplication, QFileDialog, QComboBox
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import pyqtSlot
 import sys
-
+import BIDSConversion
+import os
 
 class App(QWidget):
+
 
     def __init__(self):
         super().__init__()
@@ -30,13 +31,22 @@ class App(QWidget):
         self.name_btn.move(230, 95)
         self.name_btn.clicked.connect(self.getStudyName)
 
+
+        self.dicomPath = QLineEdit(self)
+        self.dicomPath.move(20,140)
+        self.dicomPath.resize(200,20)
+        self.dicom_btn = QPushButton("Enter Path to Dicoms", self)
+        self.dicom_btn.move(230, 136)
+        self.dicom_btn.clicked.connect(self.getDicomPath)
+
+
         self.textA = QLabel("Multiple sessions:", self)
-        self.textA.move(20, 140)
+        self.textA.move(20, 175)
         # Menu, or "combo box" for multi-session parameter
         self.multiSess = QComboBox(self)
         self.multiSess.addItem("No")
         self.multiSess.addItem("Yes")
-        self.multiSess.move(130, 134)
+        self.multiSess.move(130, 170)
         self.multiSess.activated.connect(self.multiSession)
         #studyname_btn = QPushButton('Experiment Name', self)
         #studyname_btn.move(100,70)
@@ -44,27 +54,22 @@ class App(QWidget):
 
 
         self.inputdir_btn = QPushButton("INPUT DIRECTORY", self)
-        self.inputdir_btn.move(20, 170)
+        self.inputdir_btn.move(20, 200)
         self.inputdir_btn.clicked.connect(self.getInputDir)
 
         self.outputdir_btn = QPushButton("OUTPUT DIRECTORY", self)
-        self.outputdir_btn.move(20, 200)
+        self.outputdir_btn.move(20, 240)
         self.outputdir_btn.clicked.connect(self.getOutputDir)
 
 #        self.le = QLineEdit(self)
 #        self.le.move(130, 22)
 
-        self.dicomPath = QLineEdit(self)
-        self.dicomPath.move(20,240)
-        self.dicomPath.resize(200,20)
-        self.dicom_btn = QPushButton("Enter Path to Dicoms", self)
-        self.dicom_btn.move(230, 235)
-        self.dicom_btn.clicked.connect(self.getDicomPath)
+
 
 
         self.startProcess_btn = QPushButton("START CONVERSION", self)
         self.startProcess_btn.move(15, 290)
-        self.startProcess_btn.connect(self.startConversion)
+    #    self.startProcess_btn.connect(self.startConversion)
 
         self.show()
 
@@ -72,27 +77,40 @@ class App(QWidget):
     def getOutputDir(self):
         OUTPUTDIR = str(QFileDialog.getExistingDirectory(self, "Select Directory"))
         print("---------------------> OUTPUT DIRECTORY: ", OUTPUTDIR)
-
+        BIDSConversion.setOUTPUTDIR(OUTPUTDIR)
         #self.setPixmap(QPixmap(OUTPUTDIR))
     def getInputDir(self):
         INPUTDIR = str(QFileDialog.getExistingDirectory(self, "Select Directory"))
         print("---------------------> INPUT DIRECTORY: ", INPUTDIR)
-
+        BIDSConversion.setINPUTDIR(INPUTDIR)
     @pyqtSlot()
     def multiSession(self):
+        MULTISESS = self.multiSess.currentText()
         print("CURRENT CHOICE: ", self.multiSess.currentText())
+        if MULTISESS == 'Yes':
+            BIDSConversion.setMULTISESS(True)
+        else:
+            BIDSConversion.setMULTISESS(False)
 
     def getStudyName(self):
         STUDYNAME = self.studyName.text()
-        QMessageBox.question(self, 'Message - pythonspot.com', "You typed: " + STUDYNAME, QMessageBox.Ok, QMessageBox.Ok)
-        self.studyName.setText("")
+        print("---------------------> STUDYNAME: ", STUDYNAME)
+        BIDSConversion.setSTUDYNAME(STUDYNAME)
+
+
+
+        #QMessageBox.question(self, 'Message - pythonspot.com', "You typed: " + STUDYNAME, QMessageBox.Ok, QMessageBox.Ok)
+        #self.studyName.setText("")
     def getDicomPath(self):
         DICOMPATH = self.dicomPath.text()
-        QMessageBox.question(self, 'Message - pythonspot.com', "You typed: " + DICOMPATH, QMessageBox.Ok, QMessageBox.Ok)
-        self.dicomPath.setText("")
-    def startConversion(self):
-        if 
+        print("---------------------> DICOMPATH: ", DICOMPATH)
+        BIDSConversion.setDICOMPATH(DICOMPATH)
 
+
+        #QMessageBox.question(self, 'Message - pythonspot.com', "You typed: " + DICOMPATH, QMessageBox.Ok, QMessageBox.Ok)
+        #self.dicomPath.setText("")
+    #def startConversion(self):
+        #if
 
 """
     def on_click(self):
